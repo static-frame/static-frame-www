024984f852205f98549b9a4e029f3c3a6d7bbfa9
Christopher Ariza <ariza@flexatone.com>
2022-07-27 12:59:06 -0700

    preliminary integration of tailwind

--- site-static-frame/src/App.tsx
+++ site-static-frame/src/App.tsx
@@ -8,22 +8,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>StaticFrame: A library of immutable and grow-only Pandas-like DataFrames with a more explicit and consistent interface.
+        <p className="text-1xl font-sans">StaticFrame: A library of immutable and grow-only Pandas-like DataFrames with a more explicit and consistent interface.
         </p>
         <a
-          className="App-link"
+          className="text-1xl font-sans text-slate-500"
           href="https://github.com/InvestmentSystems/static-frame"
           target="_blank"
           rel="noopener noreferrer"
         >Code</a>
         <a
-          className="App-link"
+          className="text-1xl font-sans text-slate-500"
           href="https://static-frame.readthedocs.io/en/latest/"
           target="_blank"
           rel="noopener noreferrer"
         >Documentation</a>
         <a
-          className="App-link"
+          className="text-1xl font-sans text-slate-500"
           href="https://mybinder.org/v2/gh/static-frame/static-frame-ftgu/default?urlpath=tree/index.ipynb"
           target="_blank"
           rel="noopener noreferrer"
