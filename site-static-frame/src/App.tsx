acd89af25851a0d3db2174dca90ac9c3b8db913f
Christopher Ariza <ariza@flexatone.com>
2022-10-13 18:12:38 -0700

    refactoring to avoid CSS, using components for page defined

--- site-static-frame/src/App.tsx
+++ site-static-frame/src/App.tsx
@@ -1,36 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+
+
+
+function SFLogo() {
+    return (
+        <div>
+        <img src={logo} style={{height: '40vmin'}}alt="flexatone logo" />
+        </div>
+    )
+}
+
+function Description() {
+    return (
+        <div>
+        <p className="text-1xl text-zinc-600 font-sans">StaticFrame: A library of immutable and grow-only Pandas-like DataFrames with a more explicit and consistent interface.
+        </p>
+        </div>
+    )
+}
+
+interface LinkProps {
+    label: string;
+    url: string;
+}
+
+function Link({label, url}:LinkProps) {
+    return (
+        <a
+        className="text-1xl font-sans text-slate-500"
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >{label}</a>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
-        <p className="text-1xl font-sans">StaticFrame: A library of immutable and grow-only Pandas-like DataFrames with a more explicit and consistent interface.
-        </p>
-        <a
-          className="text-1xl font-sans text-slate-500"
-          href="https://github.com/InvestmentSystems/static-frame"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Code</a>
-        <a
-          className="text-1xl font-sans text-slate-500"
-          href="https://static-frame.readthedocs.io/en/latest/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Documentation</a>
-        <a
-          className="text-1xl font-sans text-slate-500"
-          href="https://mybinder.org/v2/gh/static-frame/static-frame-ftgu/default?urlpath=tree/index.ipynb"
-          target="_blank"
-          rel="noopener noreferrer"
-        >Try it now</a>
-      </header>
+  return (
+    <div className="p-8 max-w-sm mx-auto bg-black rounded-xl shadow-md flex items-center space-x-4">
+        <Description />
+        <div>
+            <p className="text-gray-500">StaticFrame</p>
+        </div>
+        <SFLogo />
+        <Link label='Code' url='https://github.com/InvestmentSystems/static-frame' />
     </div>
+
   );
 }
 
 export default App;
+
+
+
+
+//   <a
+//     className="text-1xl font-sans text-slate-500"
+//     href="https://github.com/InvestmentSystems/static-frame"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >Code</a>
+//   <a
+//     className="text-1xl font-sans text-slate-500"
+//     href="https://static-frame.readthedocs.io/en/latest/"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >Documentation</a>
+//   <a
+//     className="text-1xl font-sans text-slate-500"
+//     href="https://mybinder.org/v2/gh/static-frame/static-frame-ftgu/default?urlpath=tree/index.ipynb"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >Try it now</a>
--- site-static-frame/src/index.tsx
+++ site-static-frame/src/index.tsx
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.body.classList.add('bg-zinc-900');
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
