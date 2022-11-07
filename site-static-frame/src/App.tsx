df6feb72a242dd7e76c22cbc39b54e38a232695a
Christopher Ariza <ariza@flexatone.com>
2022-11-07 09:05:44 -0800

    design updates

--- site-static-frame/src/App.tsx
+++ site-static-frame/src/App.tsx
@@ -12,7 +12,7 @@ interface SFSVGProps {
 }
 function SFSVG({ring, infinity, frame}: SFSVGProps) {
     return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 226.77 226.77">
+    <svg xmlns="http://www.w3.org/2000/svg" width="250" height="250">
         <path d="M113.39,0A113.39,113.39,0,1,1,33.21,33.21,113,113,0,0,1,113.39,0m0,17.19a96.21,96.21,0,1,0,68,28.18A95.84,95.84,0,0,0,113.39,17.19Z" fill={ring}/>
         <path d="M156.74,91.41l3.08-.91h0l14.11-4.15v.31h0v17.57l-2.17.64h0l-15,4.41Z" fill={frame}/>
         <path d="M104.79,107.3v-.57l0-.07h0V78.26a34.64,34.64,0,0,1,69-4.23l-16.95,5c0-.25,0-.5,0-.75a17.45,17.45,0,1,0-34.89,0h0v23.33L137.76,97l7-2.06v.31h0v17.57l-2.18.64h0l-15,4.41h0L122,119.47V120l0,.08h0v28.39A34.61,34.61,0,1,1,77.6,115.3h0Zm25.84,10.2,0,.07h0Zm-25.84,31h0V125.18l-22.36,6.57h0a17.46,17.46,0,1,0,22.36,16.76Z" fill={infinity}/>
@@ -71,7 +71,7 @@ function App() {
   return (
 
     <div>
-        <div className="max-w-xxl mx-auto">
+        <div className="max-w-full mx-auto">
         <div className="-mx-4 flex flex-wrap px-2 py-2 bg-black">
             <div className={cnCol2FlexCol}>
               {/* <div className={cnColFieldGradient}>
@@ -85,7 +85,7 @@ function App() {
         </div>
 
 
-        <div className="max-w-xl mx-auto pr-8 pl-8 pt-4">
+        <div className="max-w-5xl mx-auto pr-8 pl-8 pt-4">
         <div className="-mx-4 flex flex-wrap px-2 py-2 bg-black rounded-md">
 
             <div className={cnCol2FlexCol}>
@@ -104,7 +104,7 @@ function App() {
         </div>
 
 
-        <div className="max-w-xl mx-auto pr-8 pl-8">
+        <div className="max-w-5xl mx-auto pr-8 pl-8">
         <div className="-mx-4 flex flex-wrap my-4 px-2 py-2 bg-black rounded-md">
             <div className={cnCol3FlexCol}>
               <div className={cnColFieldGradient}>
@@ -121,20 +121,6 @@ function App() {
               <Link label='Try it now' url='https://mybinder.org/v2/gh/static-frame/static-frame-ftgu/default?urlpath=tree/index.ipynb' />
               </div>
             </div>
-        </div>
-        </div>
-
-        <div className="max-w-xxl mx-auto">
-        <div className="-mx-4 flex flex-wrap px-2 py-2 h-screen bg-black">
-            <div className={cnCol2FlexCol}>
-              {/* <div className={cnColFieldGradient}>
-              </div> */}
-            </div>
-            <div className={cnCol2FlexCol}>
-              {/* <div className={cnColField}>
-              </div> */}
-            </div>
-
         </div>
         </div>
 
